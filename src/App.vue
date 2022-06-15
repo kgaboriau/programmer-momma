ce630a21961ac8c04b14958e58381e591c813322
khanson <khanson@smarttech.com>
2022-06-15 11:52:23 -0600

    font update

--- src/App.vue
+++ src/App.vue
@@ -7,18 +7,26 @@
 </template>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Work+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap');
 @import 'src/styles/colours.sass';
+@import 'src/styles/breakpoints.sass';
 
 html, body {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  height: 100%;
+  margin: 0;
+
+  font-family: 'Work Sans', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: $text-main;
-}
+  font-size: 16px;
 
-nav {
-  padding: 30px;
+  @include break-tablet-and-up {
+    font-size: 18px;
+  }
+  @include break-desktop-and-up {
+    font-size: 20px;
+  }
 }
 
 nav a {
